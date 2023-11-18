8bf60a597f9e45e4897e178375a80456bfc2b16f
paj <2213077@slu.edu.ph>
2023-11-18 16:45:49 +0800

    may inayus sa css

--- amico_system/resources/views/employee/asset_info.blade.php
+++ amico_system/resources/views/employee/asset_info.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../res/css/asset_information.css">
+        <link rel="stylesheet" href="../res/css/asset_information_employee.css">
         <link rel="stylesheet" href="../res/css/navbar.css">
 
     </head>
